6a2fecf55bf33a5ae818a8fb9d944c9883f3b2db
Pavel Basenko <p.basenko@simcord.com>
2017-10-24 16:36:25 +0300

    Сделал структуру каталогов реализаций капчи идентичной.

--- google_captcha/index.php
+++ google_captcha/index.php
@@ -1,25 +1,3 @@
-<html lang="ru">
-    <head>
-        <title>Google reCAPTCHA noCAPTCHA demo</title>
-        <meta charset="utf-8">
-		<script src='https://www.google.com/recaptcha/api.js'></script>
-    </head>
+<?php
 
-    <body>
-        <form method="post" action="verify_captcha.php">
-            <p>
-                <b>Введите комментарий:</b>
-            </p>
-
-            <p>
-                <textarea name="comment" rows="12" cols="21"></textarea>
-            </p>
-
-			<div class="g-recaptcha" data-sitekey="ваш_секретный_код_google"></div>
-			
-            <p>
-                <input type="submit" value="Отправить">
-            </p>
-        </form>
-    </body>
-</html>
+include('form.html');
